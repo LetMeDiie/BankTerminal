bdb2b0634e5336f35e7b0cc420352262d02c856e
IOOO_7 <90846158+LetMeDiie@users.noreply.github.com>
2024-07-07 19:27:57 +0500

    Update TransactionManagerInterface.java

--- src/com/letmedie/dates/TransactionManagerInterface.java
+++ src/com/letmedie/dates/TransactionManagerInterface.java
@@ -3,7 +3,10 @@ import com.letmedie.models.Transaction;
 import com.letmedie.models.User;
 import java.util.Iterator;
 
-public interface TransactionManagerInterface {
-    void addTransaction(Transaction transaction);
+public interface TransactionManagerInterface { 
+    //Adding a new transaction
+    void addTransaction(Transaction transaction);  
+    
+    //Receiving a transaction for the transferred user
     Iterator<Transaction> getTransactionsIterator(User user);
 }
