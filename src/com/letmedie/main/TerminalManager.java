81ed3fab1e3d22d0796f0bed281b523435f35d50
IOOO_7 <90846158+LetMeDiie@users.noreply.github.com>
2024-07-07 19:50:15 +0500

    Update TerminalManager.java

--- src/com/letmedie/main/TerminalManager.java
+++ src/com/letmedie/main/TerminalManager.java
@@ -4,22 +4,24 @@ import com.letmedie.commandss.Command;
 import com.letmedie.input_output.input.ConsoleInput;
 import com.letmedie.input_output.input.Input;
 
+//And this is the terminal state
 public abstract class TerminalManager {
     Terminal terminal;
-    protected Input input;
+    protected Input input;//Something like receiving data from the user
 
     public TerminalManager(Terminal terminal){
         this.terminal=terminal;
         input=createInput();
     }
     protected Input createInput(){
-        return new ConsoleInput();
+        return new ConsoleInput();  
+        //It may be a very strange decision to create this here. But I can change the behavior in subclasses
     }
 
     public void run() {
         displayCommands();
         String choice = input.requestInput("Enter command: ");
-        Command command = createCommand(choice);
+        Command command = createCommand(choice); 
         if (command != null) {
             command.execute();
         } else {
@@ -27,8 +29,8 @@ public abstract class TerminalManager {
         }
     }
 
-    protected abstract void displayCommands();
-    protected abstract Command createCommand(String commandType);
+    protected abstract void displayCommands(); //display the command
+    protected abstract Command createCommand(String commandType); //return the selected command
     public void changeTerminalManager(TerminalManager terminalManager) {
         terminal.setTerminalManager(terminalManager);
     }
