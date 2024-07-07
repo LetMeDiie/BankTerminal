da49dc3e5363f1ecd1b8f18da42b1e117d1a47b1
IOOO_7 <90846158+LetMeDiie@users.noreply.github.com>
2024-07-07 19:35:03 +0500

    Update LoginCommand.java

--- src/com/letmedie/commandss/login_commands/LoginCommand.java
+++ src/com/letmedie/commandss/login_commands/LoginCommand.java
@@ -21,11 +21,17 @@ public class LoginCommand implements Command {
 
     @Override
     public void execute() {
-        try {
-            User user = userManager.getUserByCardNumber(cardNumber);
-            userManager.verifyPinCode(user,pinCode);
-            TerminalManager newTerminalManager = new TerminalManagerUser(terminalManager.getTerminal(),user);
-            terminalManager.changeTerminalManager(newTerminalManager);
+        try { 
+            //We get the user using the transferred card number.
+            User user = userManager.getUserByCardNumber(cardNumber); 
+            
+            //If the entered card exists, check the PIN code
+            userManager.verifyPinCode(user,pinCode); 
+
+            //Until this point, the exception is not thrown? Great, the recording was successful. Then it's time to go to the User Account Control menu.
+            TerminalManager newTerminalManager = new TerminalManagerUser(terminalManager.getTerminal(),user);  
+            //change the terminal state
+            terminalManager.changeTerminalManager(newTerminalManager); 
             String result = String.format("%s, you have successfully logged into your account.",user.getUserName());
             System.out.println(result);
 
