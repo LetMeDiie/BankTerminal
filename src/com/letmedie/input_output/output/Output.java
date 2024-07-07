96831093da6954bd85738f1e2733c7e02a85b726
IOOO_7 <90846158+LetMeDiie@users.noreply.github.com>
2024-07-07 20:03:57 +0500

    Update Output.java

--- src/com/letmedie/input_output/output/Output.java
+++ src/com/letmedie/input_output/output/Output.java
@@ -1,5 +1,9 @@
 package com.letmedie.input_output.output;
 
 public interface Output {
-    public void printOutput(String message);
+    public void printOutput(String message); 
+    //It was a pleasure working with you.
+
+//I wanted to add it to the application.
+//But because of this, the class commands constructor became too large
 }
