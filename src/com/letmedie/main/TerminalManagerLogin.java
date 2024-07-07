101546afb939c48dac98b51100b4d6e0f0c3afa5
IOOO_7 <90846158+LetMeDiie@users.noreply.github.com>
2024-07-07 19:51:06 +0500

    Update TerminalManagerLogin.java

--- src/com/letmedie/main/TerminalManagerLogin.java
+++ src/com/letmedie/main/TerminalManagerLogin.java
@@ -3,14 +3,17 @@ package com.letmedie.main;
 import com.letmedie.commandss.Command;
 import com.letmedie.commandss.login_commands.ExitCommand;
 import com.letmedie.commandss.login_commands.LoginCommand;
-import com.letmedie.dates.UserManagerSimple;
+import com.letmedie.dates.UserManagerSimple; 
+
+//And this class is more like a factory than a state.
 
 public class TerminalManagerLogin extends TerminalManager {
     public TerminalManagerLogin(Terminal terminal) {
         super(terminal);
     }
 
-    protected void displayCommands() {
+    protected void displayCommands() { 
+        //commands
         System.out.println("Available commands:");
         System.out.println("1. Login");
         System.out.println("2. Exit");
@@ -18,7 +21,7 @@ public class TerminalManagerLogin extends TerminalManager {
     protected Command createCommand(String commandType) {
         switch (commandType) {
             case "1":
-                String cardNumber = this.input.requestInput("Enter card number: ");
+                String cardNumber = this.input.requestInput("Enter card number: "); 
                 String pinCode = this.input.requestInput("Enter PIN code: ");
                 return new LoginCommand(this,new UserManagerSimple(),cardNumber,pinCode);
             case "2":
