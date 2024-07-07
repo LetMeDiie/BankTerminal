67cbe5946408dc0a4257ec3af450ac96de0f34c9
IOOO_7 <90846158+LetMeDiie@users.noreply.github.com>
2024-07-07 19:29:40 +0500

    Update UserManager.java

--- src/com/letmedie/dates/UserManager.java
+++ src/com/letmedie/dates/UserManager.java
@@ -5,6 +5,9 @@ import com.letmedie.models.User;
 import javax.naming.AuthenticationException;
 
 public interface UserManager {
-    public User getUserByCardNumber(String cardNumber) throws AuthenticationException;
+    //Retrieving a user by card number.
+    public User getUserByCardNumber(String cardNumber) throws AuthenticationException; 
+
+    //Checking the entered PIN code. Only required for authentication
     public void verifyPinCode(User user,String pinCode) throws AuthenticationException;
 }
